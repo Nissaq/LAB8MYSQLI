10d53a5c7c373d3ba1e50cf31be00e28cf2b36a9
Nissaq <2020470222@student.uitm.edu.my>
2022-06-07 00:14:45 +0800

    conn.php

---
+++ conn.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    define('db_user','root');
+    define('db_password','');
+    define('db_host','localhost');
+    define('db_name','registration');
+
+    //Create connection
+    $conn = mysqli_connect(db_host, db_user, db_password, db_name);
+
+    //Check connection 
+    if (mysqli_connect_error()){
+
+        echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
+    } 
+
+    ?>
+</body>
+</html>
