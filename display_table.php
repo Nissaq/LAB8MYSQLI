bf5f5b4aa0b82256bff8f6b35b4bff4815bc551f
Nissaq <2020470222@student.uitm.edu.my>
2022-06-07 00:38:54 +0800

    display_table.php

--- display_table.php
+++ display_table.php
@@ -10,7 +10,7 @@
     <?php
     include 'conn.php';
 
-    echo "<table border='1'><tr><th>First Name</th><th>Last Namr</th>";
+    echo "<table border='1'><tr><th>First Name</th><th>Last Name</th>";
     echo "<th>E-mal</th><th>Password</th><th>Registration Date</th></tr>";
 
     $result = mysqli_query($conn, "select * from user");
